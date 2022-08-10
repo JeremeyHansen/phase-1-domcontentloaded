97486b301d59841a13ae5440706b185d084c91a5
Jeremey Hansen <jeremeyhansen@yahoo.com>
2022-08-10 18:47:08 -0400

    commit tree 1938c7ff179c25191a5a8497a4ec3813ca3c6796

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    // console.log("The DOM has loaded");
+    // https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector
+    // https://developer.mozilla.org/en-US/docs/Web/API/Node
+    document.querySelector('p').textContent = 'This is really cool!';
+    // document.querySelector('#text') // changed the text
+    
+  });
